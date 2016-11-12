b1d6ef17c32d20e53f5dae22e0ada5161ba1beb3
Lucas Fonseca dos Santos <fonsecalucassantos@gmail.com>
2016-11-12 00:49:35 -0200

    Commit 01: Estruturas de diretorio, classe Principal (contem main) e javadoc

---
+++ br/ufla/dcc/gcc110/s2016_02/trabalho1/HueIncendiario/src/Principal.java
@@ -0,0 +1,15 @@
+package br.ufla.dcc.gcc110.s2016_02.trabalho1.HueIncendiario;
+
+/**
+  * Classe responsavel pela execução do software.
+  * Contém o metodo main para ser chamado executando toda a computação.
+  * @author Lucas Fonseca dos Santos
+  *
+  */
+public class Principal {
+
+  public static void main(String[] args) {
+
+  }
+
+}
